2c310c6a3176a7928b937befebbdc35b24d4b95a
Ugitch <ugison@gmail.com>
2018-10-26 04:43:13 +0200

    Modify square function;add prototype

--- square.c
+++ square.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
-double square(double x) {
-    return x*x;
-}
+double square(double);
 
 int main() {
     double a = 2.5;
     double aSquared = square(a);
     printf("%.2lf squared = %.2lf\n", a, aSquared);
     return 0;
+}
+
+double square(double a) {
+    return a*a;
 }
