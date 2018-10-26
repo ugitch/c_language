60cb837671557f470d0b07dcf795a29264da5a29
Ugitch <ugison@gmail.com>
2018-10-26 13:47:27 +0200

    Add square solution using math library

--- square.c
+++ square.c
@@ -1,13 +1,9 @@
+#include <math.h>
 #include <stdio.h>
-double square(double);
 
 int main() {
     double a = 2.5;
-    double aSquared = square(a);
+    double aSquared = pow(a, 2);
     printf("%.2lf squared = %.2lf\n", a, aSquared);
     return 0;
-}
-
-double square(double a) {
-    return a*a;
 }
