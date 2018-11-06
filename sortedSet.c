3356b440d4e091a0000da538dd220180f6ea6e1d
Ugitch <ugison@gmail.com>
2018-11-06 18:57:39 +0100

    Add \n in printuot

--- sortedSet.c
+++ sortedSet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    //! showArray(list, cursors=[i])
+    //! showArray(list, cursors=[i,j])
     // Sorting from smallest to largest
     int list[] = {759, 14, 2, 900, 106, 77, -10, 8, -3, 5}; /* unsorted list */
     int n = 10;
     int i, j, index, min;
     int swap;
     
-    for (i=0; i<=n-2; i++) {
+    for (i=0; i<n-1; i++) {
         index = i;
         min = list[i];
-        for (j=i+1; j<=n-1; j++) {
+        for (j=i+1; j<n; j++) {
             if (list[j] < min) {
                 min = list[j];
                 index = j;
@@ -27,6 +27,7 @@ int main(void) {
     printf("\nSorted list: \n");
     for (i=0; i<n; i++)
         printf("%d ", list[i]);
+    printf("\n");
 
     return 0;
 }
